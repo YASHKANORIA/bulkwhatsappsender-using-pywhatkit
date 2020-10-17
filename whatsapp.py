ce55d4c4daccd146dd06b92d070a6a41d44f179e
YASHKANORIA <YSHKANORIA@HOTMAIL.COM>
2020-10-18 01:41:34 +0530

    Add files via upload

--- whatsapp.py
+++ whatsapp.py
@@ -1,7 +1,13 @@
 import pywhatkit as kit
 import pandas as pd
 from datetime import datetime
+import pandas as pd
 
+#csv Data
+data = pd.read_csv("E:\python\contact.csv")
+data_dict = data.to_dict('list')
+leads = data_dict['Number']
+print (leads)
 
 now = datetime.now()
 
@@ -9,22 +15,25 @@ now = datetime.now()
 text='hey test'
 
 
-
-mylist =["+919903028438 ","+919831285176"]
-print (mylist)
+#by adding number here
+# mylist =["+919903028438 ","+919831285176","+917003266100"]
+# print (mylist)
 
 
 j=0;
-for i in mylist:
+for i in leads:
     hour = now.strftime("%H")
     min = now.strftime("%M")
     mini = int(min) + 2 +j
-    j=0
-    kit.sendwhatmsg(i, text, int(hour), mini)
+    #without using csv file
+
+    # kit.sendwhatmsg(i, text, int(hour), mini)
+    #using Csv File
+    kit.sendwhatmsg("+91"+str(i), text, int(hour), mini)
     j=j+1
 
 
-#kit.sendwhatmsg("+919831285176", "I love studytonight.com!",00 , )
+#Yash Kanoria
 
 
 
